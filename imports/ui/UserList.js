d9a4c6456775f1df593b1ea7d7b75f57211a7e72
cchanEG <cchan@evergage.com>
2018-12-06 22:12:00 -0500

    Add key to users in UserList

--- imports/ui/UserList.js
+++ imports/ui/UserList.js
@@ -3,7 +3,7 @@ import User from './User';
 
 const UserList = (props) => {
     return props.users.map((user) => {
-        return <User user={user} />;
+        return <User key={user._id} user={user} />;
     });
 };
 
