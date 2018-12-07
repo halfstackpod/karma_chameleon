1daa941b0a568c06f6145d1e9fee18c9eac6bce4
David Estrich <destrich@evergage.com>
2018-12-06 20:29:47 -0500

    Add static html - implement app componenet

--- client/main.js
+++ client/main.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 import {Tracker} from 'meteor/tracker';
 
+import App from './../imports/ui/App';
+
 Meteor.startup(() => {
   Tracker.autorun(() => {
     ReactDOM.render(<App/>, document.getElementById('app'));
