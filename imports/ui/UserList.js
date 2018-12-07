00fbc98cc00ca0b9c3cd178c37a2ca7616fcbcaf
cchanEG <cchan@evergage.com>
2018-12-06 21:56:10 -0500

    UserList component - first commit

---
+++ imports/ui/UserList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import User from './User';
+
+const UserList = (props) => {
+    return props.users.map((user) => {
+        return <User user={user} />;
+    });
+};
+
+export default UserList;
