748fb4cf483e8ed7db5b83464162da1816589e6b
cohendaniel <danielcohen376@gmail.com>
2019-01-17 22:37:28 -0500

    keep order state

--- imports/ui/karmaController/UserListContainer.js
+++ imports/ui/karmaController/UserListContainer.js
@@ -9,14 +9,16 @@ export default class UserListContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            userKarmaList: []
+            userKarmaList: [],
+            sorted: ""
         }
     }
 
     componentDidMount() {
         this.karmaTracker = Tracker.autorun(() => {
             Meteor.subscribe('userKarmaPublish');
-            const userKarmaList = userKarma.find().fetch();
+            let userKarmaList = userKarma.find().fetch();
+            userKarmaList = this.sortUsers(userKarmaList, this.state.sorted);
             console.log(userKarmaList);
             this.setState({ userKarmaList });
         });
@@ -26,34 +28,63 @@ export default class UserListContainer extends React.Component {
         this.karmaTracker.stop();
     }
 
-    getTopUsers = () => {
-        const orderedList = this.state.userKarmaList.sort((u1, u2) => {
-            return u2.karma - u1.karma
-        });
-        this.setState({userKarmaList: orderedList})
+    sortUsers = (list, sortedValue) => {
+        let sortedList = [];
+        if (sortedValue === "top" || !sortedValue) {
+            sortedList = this.getTopUsers(list)
+        } else if (sortedValue === "bottom") {
+            sortedList = this.getBottomUsers(list)
+        } else if (sortedValue === "alpha") {
+            sortedList = this.getAlphabeticalUsers(list)
+        }
+        this.setState({sorted: sortedValue});
+        return sortedList;
     }
 
-    getBottomUsers = () => {
-        const orderedList = this.state.userKarmaList.sort((u1, u2) => {
+    sortUsersChange = (event) => {
+        const userKarmaList = this.state.userKarmaList
+        this.sortUsers(userKarmaList, event.target.value)
+    }
+
+    getAlphabeticalUsers = (list) => {
+        return list.sort((u1, u2) => {
+            const name1 = u1.alias.toLowerCase()
+            const name2 = u2.alias.toLowerCase()
+            if (name1 < name2) {
+                return -1
+            } else if (name1 > name2) {
+                return 1
+            } else {
+                return 0
+            }
+        });
+    }
+
+    getTopUsers = (list) => {
+        return list.sort((u1, u2) => {
+            return u2.karma - u1.karma
+        });
+    }
+
+    getBottomUsers = (list) => {
+        return list.sort((u1, u2) => {
             return u1.karma - u2.karma
         });
-        this.setState({userKarmaList: orderedList})
     }
     
     render() {
         return (
             <div>
+                <div>
+                    <select type="select" onChange={this.sortUsersChange}>
+                        <option value="top">Top Users</option>
+                        <option value="bottom">Bottom Users</option>
+                        <option value="alpha">Alphabetical</option>
+                    </select>
+                </div>
                 <UserList 
                     userKarmaList={this.state.userKarmaList}
                 />
-                <div className="TopUsers">
-                    <button type="button" onClick={this.getTopUsers}>
-                        Top Users!
-                    </button>
-                    <button type="button" onClick={this.getBottomUsers}>
-                        Bottom Users!
-                    </button>
-                </div>
             </div>
         )
     }
